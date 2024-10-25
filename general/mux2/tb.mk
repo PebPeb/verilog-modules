936e527e3f2ceb66afd01764277a9edf451fd6a3
PebPeb <67885698+PebPeb@users.noreply.github.com>
2024-10-24 23:36:04 -0400

    Moved Testbenches to sims folder

--- general/mux2/tb.mk
+++ general/mux2/tb.mk
@@ -5,5 +5,5 @@ BUILD_NAME = mux2
 RELATIVE_PROJECT_PATH = general/mux2
 PDIR = $(PROJECT_ROOT)/$(RELATIVE_PROJECT_PATH)
 
-TB_SOURCE = $(PDIR)/mux2_tb.v $(PDIR)/mux2_tb.cpp
+TB_SOURCE = $(PDIR)/sims/mux2_tb.v $(PDIR)/sims/mux2_tb.cpp
 TB_INCLUDE = $(PDIR)/mux2.v
